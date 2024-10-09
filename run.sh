144aa6e3f61042141cdd31feef55da46824270fc
dragos <dragos.balan@adswizz.com>
2024-10-09 14:47:49 +0300

    rdd partition vs dataset.writer.partitionBy

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 JAR_PATH=$(ls -1tr target/SparkTutorial*.jar | head -n1)
 
 echo $JAR_PATH
 
 spark-submit \
-        --class "com.github.sparktutorial.dataframes.SqlTutorial" \
+        --class "com.github.sparktutorial.RddPartitioningVsDatasetWriterPartitioning" \
         --master "local[*]" \
         --driver-memory 10g \
         --executor-memory 10g \
